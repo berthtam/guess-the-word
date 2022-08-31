4f762c63f8a45cc20edbe9163d452031d7f55426
Tamber <berthtam@gmail.com>
2022-08-31 14:26:35 -0700

    guess-the-word

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = []:
 
 const placeholder = function (word) {
   const placeholderLetters = [];
@@ -22,7 +23,35 @@ placeholder(word);
 
 guessLetterButton.addEventListener("click", function (e) {
     e.preventDefault();
+    message.innerText= "";
     const guess = letterInput.value;
-    console.log(guess);
+    const goodGuess - validateInput(guess);
+
+    if (goodGuess) {
+    makeGuess(guess);
+    }
     letterInput.value = "";
 });
+
+const validateInput = function (input) {
+  const acceptedLetter = /[a-zA-Z]/;
+  if (input.length === 0) {
+  message.innerText = "Please select a letter.";
+} else if (input.length > 1) {
+  message.innerText = "Please select one letter.";
+} else if (match.length(acceptedLetter)) {
+  message.innerText = "Please select a letter from A to Z";
+} else {
+  return input;
+}
+};
+
+const makeGuess = function(guess) {
+  guess = guess.toUpperCase();
+  if (guessLetters.includes(guess)) {
+    message.innerText = "You already guessed letter. Please try again.";
+  } else {
+    guessedLetters.push(guess);
+    console.log(guessLetters);
+  }
+};
